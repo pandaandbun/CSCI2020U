d01746c0b5093850d46bc7deade11beba4e691bf
katanaWieldingPanda <Rico0Sandard@gmail.com>
2019-02-08 12:19:25 -0500

    Add lab 3 and lab 4

---
+++ lab04/lab04/src/lab04/registration.java
@@ -0,0 +1,157 @@
+package lab04;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.scene.control.DatePicker;
+
+
+public class registration extends Application {
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Lab 04 Solution");
+
+        // Create the registration form grid pane
+        GridPane gridPane = createRegistrationFormPane();
+        // Add UI controls to the registration form grid pane
+        addUIControls(gridPane);
+        // Create a scene with registration form grid pane as the root node
+        Scene scene = new Scene(gridPane, 800, 500);
+        // Set the scene in primary stage	
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+    }
+
+
+    private GridPane createRegistrationFormPane() {
+        // Instantiate a new Grid Pane
+        GridPane gridPane = new GridPane();
+
+        // Position the pane at the center of the screen, both vertically and horizontally
+        gridPane.setAlignment(Pos.CENTER);
+
+        // Set a padding of 20px on each side
+        gridPane.setPadding(new Insets(40, 40, 40, 40));
+
+        // Set the horizontal gap between columns
+        gridPane.setHgap(10);
+
+        // Set the vertical gap between rows
+        gridPane.setVgap(10);
+
+        // Add Column Constraints
+
+        // columnOneConstraints will be applied to all the nodes placed in column one.
+        ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
+        columnOneConstraints.setHalignment(HPos.RIGHT);
+
+        // columnTwoConstraints will be applied to all the nodes placed in column two.
+        ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
+        columnTwoConstrains.setHgrow(Priority.ALWAYS);
+
+        gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
+
+        return gridPane;
+    }
+
+    private void addUIControls(GridPane gridPane) {
+        
+    	// Add Name Label
+        Label userLabel = new Label("Username : ");
+        gridPane.add(userLabel, 0,0);
+
+        // Add Name Text Field
+        TextField userField = new TextField();
+        userField.setPrefHeight(40);
+        gridPane.add(userField, 1,0);
+        
+        // Add Password Label
+        Label passwordLabel = new Label("Password : ");
+        gridPane.add(passwordLabel, 0, 1);
+
+        // Add Password Field
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPrefHeight(40);
+        gridPane.add(passwordField, 1, 1);
+
+        // Add Name Label
+        Label nameLabel = new Label("Full Name : ");
+        gridPane.add(nameLabel, 0,2);
+
+        // Add Name Text Field
+        TextField nameField = new TextField();
+        nameField.setPrefHeight(40);
+        gridPane.add(nameField, 1,2);
+
+
+        // Add Email Label
+        Label emailLabel = new Label("E-Mail: ");
+        gridPane.add(emailLabel, 0, 3);
+
+        // Add Email Text Field
+        TextField emailField = new TextField();
+        emailField.setPrefHeight(40);
+        gridPane.add(emailField, 1, 3);
+        
+        // Add Phone Label
+        Label phoneLabel = new Label("Phone #: ");
+        gridPane.add(phoneLabel, 0, 4);
+
+        // Add Phone Text Field
+        TextField phoneField = new TextField();
+        phoneField.setPrefHeight(40);
+        gridPane.add(phoneField, 1, 4);
+        
+        // Add DoB Label
+        Label dobLabel = new Label("Date of Birth : ");
+        gridPane.add(dobLabel, 0, 5);
+
+        // Add DoB Field
+        DatePicker dobField = new DatePicker();
+        dobField.setPrefHeight(40);
+        dobField.setPrefWidth(800);
+        gridPane.add(dobField, 1, 5);
+
+        // Add Submit Button
+        Button submitButton = new Button("Register");
+        submitButton.setPrefHeight(40);
+        submitButton.setPrefWidth(100);
+        gridPane.add(submitButton, 0, 6, 2, 1);
+        GridPane.setHalignment(submitButton, HPos.CENTER);
+        GridPane.setMargin(submitButton, new Insets(20, 0,20,0));
+
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+               
+                if(emailField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your email id");
+                    return;
+                }
+   
+                showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(), "Registration Successful!", "Welcome " + nameField.getText());
+            }
+        });
+    }
+
+    private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
